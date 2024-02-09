abbb43eaeb6629dada7a620246d3648e67bcc6da
GrommuL <miketsvg@gmail.com>
2024-02-09 09:38:08 +0600

    feat: add delete-server-modal and functionality

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -2,6 +2,28 @@ import { database } from '@/lib/database'
 import { currentProfile } from '@/services/profile'
 import { NextResponse } from 'next/server'
 
+export async function DELETE(request: Request, { params }: { params: { serverId: string } }) {
+	try {
+		const profile = await currentProfile()
+
+		if (!profile) {
+			return new NextResponse('Unauthorized', { status: 401 })
+		}
+
+		const server = await database.server.delete({
+			where: {
+				id: params.serverId,
+				profileId: profile.id
+			}
+		})
+
+		return NextResponse.json(server)
+	} catch (error) {
+		console.log('[SERVER_ID_DELETE]', error)
+		return new NextResponse('Internal Error', { status: 500 })
+	}
+}
+
 export async function PATCH(request: Request, { params }: { params: { serverId: string } }) {
 	try {
 		const profile = await currentProfile()
